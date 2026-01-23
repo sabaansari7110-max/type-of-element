ba520fc19ca0c64b4f38e16eb5486c252ce3fc17
Saba Ansari <sabaansari7110@gmail.com>
2026-01-23 18:44:27 +0530

    Update movies names store in list.py

--- movies names store in list.py
+++ movies names store in list.py
@@ -1,3 +1,4 @@
+1st method:-
 movies= []
 mov1= input("enter 1st movie:")
 mov2= input("enter 2nd movie:")
@@ -6,5 +7,5 @@ mov3= input("enter 3rd movie:")
 movies.append(mov1)
 movies.append(mov2)
 movies.append(mov3)
-
 print(movies)
+
